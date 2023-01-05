bb4a68e3bcaca92402c8f55d9e725ecd8ca2ae75
MasterLaplace <guillauem.papineau@epitech.eu>
2023-01-05 11:42:23 -0500

    Add: some tasks

--- DoOp.hs
+++ DoOp.hs
@@ -5,7 +5,10 @@
 -- DoOp
 -}
 
-import My ( myFoldl, myLength )
+myFoldl :: (b -> a -> b) -> b -> [a] -> b
+myFoldl lambda x [] = x
+myFoldl lambda x (y:ys) = myFoldl lambda (lambda x y) ys
+
 
 myElem :: Eq a => a -> [a] -> Bool
 myElem y = myFoldl (\acc x -> (x == y) || acc) False
@@ -17,7 +20,7 @@ safeDiv x y = Just (x `div` y)
 safeNth :: [a] -> Int -> Maybe a
 safeNth [] _ = Nothing
 safeNth (x:_) 0 = Just x
-safeNth (_:xs) nth | nth > myLength xs || nth < 0 = Nothing
+safeNth (_:xs) nth | nth > length xs || nth < 0 = Nothing
                 | otherwise = safeNth xs (nth - 1)
 
 safeSucc :: Maybe Int -> Maybe Int
@@ -33,3 +36,16 @@ maybeDo :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
 maybeDo lambda Nothing _ = Nothing
 maybeDo lambda _ Nothing = Nothing
 maybeDo lambda (Just x) (Just y) = Just lambda <*>Just x <*> Just y
+
+
+isNum :: [Char] -> Bool
+isNum = foldr (\ x -> (&&) (myElem x "-0123456789")) True
+
+readInt :: [Char] -> Maybe Int
+readInt [] = Nothing
+readInt (x:xs) | isNum (x:xs) = Just (read (x:xs) :: Int)
+    | otherwise = Nothing
+
+getLineLength :: IO Int
+getLineLength = do length <$> getLine
+
